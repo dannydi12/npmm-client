358fc757b27eb5301e0cda63fa2e07f240250523
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-05 09:03:50 -0700

    added redux prematurely

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+// import counterReducer from './[componentFolder]/componentSlice.js';
+
+export default configureStore({
+  reducer: {
+    // counter: counterReducer,
+  },
+});
