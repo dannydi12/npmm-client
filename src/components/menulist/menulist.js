52f1e355beb317a23949682611266b40010ff492
Josh Young <joshua@young.net>
2020-05-08 09:00:36 -0600

    MenuList Rename

--- src/components/menulist/menulist.js
+++ src/components/menulist/menulist.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React from 'components/MenuList/node_modules/react';
+import { useSelector, useDispatch } from 'components/MenuList/node_modules/react-redux';
+import { Link } from 'components/MenuList/node_modules/react-router-dom';
 / import {all the reducers/actions} from './sliceFile.js';
 // import styles from './example.css';
 
 // Use absolute imports like this for components!
 // import SearchBox from 'components/SearchBox/SearchBox';
 
-function SideBar() {
+function MenuList() {
   // const varName = useSelector((state) => state.specific.thing.i.want); // to get stuff from state
   // const dispatch = useDispatch(); // to dispatch actions
 
   return ( 
-    <Link to="/home">Home</Link>
+    <Link to="/">Home</Link>
     <Link to="/logout">Logout</Link>
     <Link to="/collections">Collections</Link>
     <Link to="/favourites">Favourites</Link>
