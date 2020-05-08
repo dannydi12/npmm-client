80d4bb14f05a28ea6915bad26410cf0902c16d29
Josh Young <joshua@young.net>
2020-05-08 09:14:59 -0600

    small fixes to menulist to test

--- src/components/menulist/menulist.js
+++ src/components/menulist/menulist.js
@@ -1,20 +1,22 @@
-import React from 'components/MenuList/node_modules/react';
-import { useSelector, useDispatch } from 'components/MenuList/node_modules/react-redux';
-import { Link } from 'components/MenuList/node_modules/react-router-dom';
-/ import {all the reducers/actions} from './sliceFile.js';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+// import {all the reducers/actions} from './sliceFile.js';
 // import styles from './example.css';
 
 // Use absolute imports like this for components!
 // import SearchBox from 'components/SearchBox/SearchBox';
 
-function MenuList() {
+export default function MenuList() {
   // const varName = useSelector((state) => state.specific.thing.i.want); // to get stuff from state
   // const dispatch = useDispatch(); // to dispatch actions
 
-  return ( 
-    <Link to="/">Home</Link>
-    <Link to="/logout">Logout</Link>
-    <Link to="/collections">Collections</Link>
-    <Link to="/favourites">Favourites</Link>
-   );
+  return (
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/logout">Logout</NavLink>
+      <NavLink to="/collections">Collections</NavLink>
+      <NavLink to="/favourites">Favourites</NavLink>
+    </>
+  );
 }
