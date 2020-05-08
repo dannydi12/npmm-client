0398369db5da32f0e52d035372c798f625721996
Vik <lifeasvik@gmail.com>
2020-05-07 23:36:02 -0500

    started sidebar

--- src/components/menulist/menulist.js
+++ src/components/menulist/menulist.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import {all the reducers/actions} from './sliceFile.js';
+/ import {all the reducers/actions} from './sliceFile.js';
 // import styles from './example.css';
 
 // Use absolute imports like this for components!
 // import SearchBox from 'components/SearchBox/SearchBox';
 
-function NavBar() {
+function SideBar() {
   // const varName = useSelector((state) => state.specific.thing.i.want); // to get stuff from state
   // const dispatch = useDispatch(); // to dispatch actions
 
-  return (
-    
-    </div>
-  );
+  return ( 
+    <Link to="/home">Home</Link>
+    <Link to="/logout">Logout</Link>
+    <Link to="/collections">Collections</Link>
+    <Link to="/favourites">Favourites</Link>
+   );
 }
-
-export default NavBar;
