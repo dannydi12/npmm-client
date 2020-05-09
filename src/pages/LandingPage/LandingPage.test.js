da30110c1f791abcb4047b7f66a83876b2b27221
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-09 16:05:03 -0600

    Trying to get mock to work for useHistory hook

--- src/pages/LandingPage/LandingPage.test.js
+++ src/pages/LandingPage/LandingPage.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactRouterDOM from 'react-router-dom';
 import LandingPage from './LandingPage';
+
+jest.mock('react-router-dom', () => {
+  const actual = ReactRouterDOM;
+  return {
+    ...actual,
+    useHistory: () => ({ methods }),
+  };
+});
 
 test('renders LandingPage without crashing', () => {
   const div = document.createElement('div');
