bed1b13b72ac1c0b604be0807bda593bc9b5f2bd
Josh Young <joshua@young.net>
2020-05-09 13:13:33 -0600

    search and navbar done

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import {all the reducers/actions} from './sliceFile.js';
-// import styles from './example.css';
-
-// Use absolute imports like this for components!
-// import SearchBox from 'components/SearchBox/SearchBox';
+import SearchBox from '../../components/SearchBox/SearchBox';
+import NavBar from '../../components/NavBar/NavBar';
 
 function LandingPage() {
-  // const varName = useSelector((state) => state.specific.thing.i.want); // to get stuff from state
-  // const dispatch = useDispatch(); // to dispatch actions
-
   return (
     <div>
-      <p>stuff</p>
+      <NavBar />
+      <h1 className="landingTitle">NPMM</h1>
+      <h2 className="landingFullName">Node Package Manager Manager</h2>
+      <SearchBox classProps="landingSearch" />
     </div>
   );
 }
