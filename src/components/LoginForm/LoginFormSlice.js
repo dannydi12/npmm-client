a80fd4b09a9d5ee31245f79d6edac85602e2a4cf
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-09 12:42:35 -0600

    Move loginPageSlice code to LoginFormSlice

--- src/components/LoginForm/LoginFormSlice.js
+++ src/components/LoginForm/LoginFormSlice.js
@@ -1,33 +1,18 @@
 /* eslint-disable no-param-reassign */
 import { createSlice } from '@reduxjs/toolkit';
 
-export const counterSlice = createSlice({
-  name: 'counter',
+export const loginSlice = createSlice({
+  name: 'login',
   initialState: {
-    value: 0,
-    amount: 2,
+    currentUser: false,
   },
   reducers: {
-    increment: (state) => {
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state) => {
-      state.value += state.amount;
-    },
-    changeAmount: (state, action) => {
-      state.amount = Number(action.payload);
+    loginUser: (state) => {
+      state.currentUser = true;
     },
   },
 });
 
-export const {
-  increment,
-  decrement,
-  incrementByAmount,
-  changeAmount,
-} = counterSlice.actions;
+export const { loginUser } = loginSlice.actions;
 
-export default counterSlice.reducer;
+export default loginSlice.reducer;
