4045c61c740ef65f8d2f6538729d9f9441ceb339
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-09 11:46:36 -0700

    started work on API

--- src/pages/SearchResultPage/SearchResultPage.js
+++ src/pages/SearchResultPage/SearchResultPage.js
@@ -1,16 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
+import { useSelector, useDispatch } from 'react-redux';
 import PackageList from '../../components/PackageList/PackageList';
+import { fetchPackages } from './SearchResultPageSlice';
 // import styles from './example.css';
 
 function SearchResultPage() {
-  const [packs, setPacks] = useState([]); // maybe use state slice instead
+  const dispatch = useDispatch();
   const location = useLocation();
-  const parsed = queryString.parse(location.search);
-  console.log(parsed);
 
-  useEffect(() => {});
+  const parsed = queryString.parse(location.search);
+
+  const packs = useSelector((state) => state.searchResults.packs);
+
+  useEffect(() => {
+    dispatch(fetchPackages(parsed.q));
+  });
 
   return (
     <section>
