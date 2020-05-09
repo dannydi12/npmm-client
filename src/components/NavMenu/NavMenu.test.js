42618343498af29465cf7b8a0015450ab9292a81
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-09 15:18:38 -0600

    Implement NavMenu smoke test

---
+++ src/components/NavMenu/NavMenu.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
+import NavMenu from './NavMenu';
+
+test('renders NavMenu without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavMenu />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
