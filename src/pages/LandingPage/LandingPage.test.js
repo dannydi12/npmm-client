c7b5e811fa9eb0eeadbb78f05350ce07e024b42f
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-11 19:40:02 -0600

    Fixed test with hooks

--- src/pages/LandingPage/LandingPage.test.js
+++ src/pages/LandingPage/LandingPage.test.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import ReactRouterDOM from 'react-router-dom';
 import LandingPage from './LandingPage';
 
-// Doesn't work but is shown as one method to get test to work
-jest.mock('react-router-dom', () => {
-  const actual = ReactRouterDOM;
-  return {
-    ...actual,
-    useHistory: () => ({ methods }),
-  };
-});
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: () => ({ push: jest.fn() })
+}));
 
 test('renders LandingPage without crashing', () => {
   const div = document.createElement('div');
