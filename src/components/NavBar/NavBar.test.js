08a331cc6cfc20b56ca6eaddaac07a9354f243ae
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-11 19:48:49 -0600

    Fix NavBar and SearchBox test

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
 import NavBar from './NavBar';
+
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: () => ({ push: jest.fn() }),
+}));
 
 test('renders navbar without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<NavBar />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/SearchBox/SearchBox.test.js
+++ src/components/SearchBox/SearchBox.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBox from './SearchBox';
 
+jest.mock('react-router', () => ({
+  ...jest.requireActual('react-router'),
+  useHistory: () => ({ push: jest.fn() })
+}));
+
 test('renders SearchBox without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<SearchBox />, div);
