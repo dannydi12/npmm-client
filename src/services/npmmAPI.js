2a9573c9dbafbe491f5b912031b8b6103e7e45b6
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-12 11:50:58 -0700

    added several API calls for later use

--- src/services/npmmAPI.js
+++ src/services/npmmAPI.js
@@ -20,9 +20,76 @@ const npms = {
         return 'There was an issue';
       });
   },
-  getCollections: () => {
+  getCollections: (type = undefined) => {
+    return fetch(
+      `${config.API_ENDPOINT}/api/collections${type && `?type=${type}`}`,
+      {
+        method: 'get',
+        headers: {
+          Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((packs) => packs)
+      .catch((err) => {
+        return 'There was an issue';
+      });
+  },
+  createCollection: (name, isLaunchPad = false) => {
     return fetch(`${config.API_ENDPOINT}/api/collections`, {
-      method: 'get',
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        isLaunchPad,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((collection) => collection)
+      .catch((err) => {
+        return 'There was an issue';
+      });
+  },
+  updateCollection: (id, name, isLaunchPad) => {
+    return fetch(`${config.API_ENDPOINT}/api/collections/${id}`, {
+      method: 'patch',
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        isLaunchPad,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((collection) => collection)
+      .catch((err) => {
+        return 'There was an issue';
+      });
+  },
+  deleteCollection: (id) => {
+    return fetch(`${config.API_ENDPOINT}/api/collections/${id}`, {
+      method: 'delete',
       headers: {
         Authorization: `Bearer ${TokenService.getAuthToken()}`,
       },
@@ -33,7 +100,53 @@ const npms = {
         }
         return res.json();
       })
-      .then((packs) => packs)
+      .then((collection) => collection)
+      .catch((err) => {
+        return 'There was an issue';
+      });
+  },
+  addPackage: (name, collectionId) => {
+    return fetch(`${config.API_ENDPOINT}/api/packages`, {
+      method: 'post',
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        collectionId,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((collection) => collection)
+      .catch((err) => {
+        return 'There was an issue';
+      });
+  },
+  deletePackage: (name, collectionId) => {
+    return fetch(`${config.API_ENDPOINT}/api/packages`, {
+      method: 'delete',
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        collectionId,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.message);
+        }
+        return res.json();
+      })
+      .then((collection) => collection)
       .catch((err) => {
         return 'There was an issue';
       });
