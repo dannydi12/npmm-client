dcc6100d562180afb6c06f885f90ca5da7da82b7
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-13 13:19:36 -0700

    integrated package page with rest of project

--- src/App.js
+++ src/App.js
@@ -22,9 +22,7 @@ function App() {
       <main className="appMain">
         <Switch>
           <Route exact path="/collection/:id" component={CollectionPage} />
-          <Route exact path="/package/:packageName">
-            <PackagePage />
-          </Route>
+          <Route exact path="/package/:packageName" component={PackagePage} />
           <Route exact path="/login" component={LoginPage} />
           <Route exact path="/signup" component={SignupPage} />
           <Route exact path="/search" component={SearchResultPage} />
--- src/services/npmsAPI.js
+++ src/services/npmsAPI.js
@@ -2,20 +2,14 @@ const npmsURL = 'https://api.npms.io';
 
 const npms = {
   searchPackages: (search) => {
-    return fetch(`${npmsURL}/v2/search?q=${encodeURI(search)}`).then((res) => {
-      if (!res.ok) {
-        Promise.reject(res.error);
-      }
-      return res.json();
-    });
+    return fetch(`${npmsURL}/v2/search?q=${encodeURI(search)}`).then((res) =>
+      !res.ok ? res.json().then((err) => Promise.reject(err)) : res.json()
+    );
   },
   getPackageInfo: (name) => {
-    return fetch(`${npmsURL}/v2/package/${encodeURI(name)}`).then((res) => {
-      if (!res.ok) {
-        Promise.reject(res.error);
-      }
-      return res.json();
-    });
+    return fetch(`${npmsURL}/v2/package/${name}`).then((res) =>
+      !res.ok ? res.json().then((err) => Promise.reject(err)) : res.json()
+    );
   },
 };
 
