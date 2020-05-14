fc6d5813130780dd2da9e1b51945d7d0116d7a6e
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-13 20:47:23 -0600

    Fix LoginPage and SearchBox test

--- src/components/SearchBox/SearchBox.test.js
+++ src/components/SearchBox/SearchBox.test.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
 import SearchBox from './SearchBox';
 
 jest.mock('react-router', () => ({
   ...jest.requireActual('react-router'),
-  useHistory: () => ({ push: jest.fn() })
+  useHistory: () => ({ push: jest.fn() }),
 }));
 
 test('renders SearchBox without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<SearchBox />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <SearchBox />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/pages/LoginPage/LoginPage.test.js
+++ src/pages/LoginPage/LoginPage.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
 import LoginPage from './LoginPage';
 
-test('renders login page without crashing', () => {
+test('renders login form without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<LoginPage />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <LoginPage />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
