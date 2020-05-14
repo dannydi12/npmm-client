51259f44096e5e853f2a31b3c29949c0471b20cc
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-13 20:15:21 -0600

    Refactor LoginForm test to handle history hook use

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '../../store';
 import LoginForm from './LoginForm';
 
 test('renders login form without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<LoginForm />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <LoginForm />
+      </BrowserRouter>
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
