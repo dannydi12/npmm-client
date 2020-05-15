4571c8a567ac96e221cac78fc0bc0434df4ad8da
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-14 20:21:50 -0600

    Fix error logging

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -7,18 +7,16 @@ export default class ErrorBoundary extends Component {
   }
 
   static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
   componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    logErrorToMyService(error, errorInfo);
+    // basic error logging to be replaced
+    console.log(error, errorInfo);
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <h1>Something went wrong.</h1>;
     }
 
