d4e599aeb71998c6d066220adcef1d560729b167
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-14 19:57:08 -0600

    Implement Logout functionality

--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import NavCollections from '../NavCollections/NavCollections';
+import TokenService from '../../services/token-service';
 // import styles from './example.css';
 
 function NavMenu() {
@@ -10,7 +11,9 @@ function NavMenu() {
   return (
     <aside className="navMenu">
       <NavLink to="/">Home</NavLink>
-      <NavLink to="/logout">Logout</NavLink>
+      <NavLink to="/logout" onClick={TokenService.clearAuthToken()}>
+        Logout
+      </NavLink>
       <NavLink to="/favorites">Favorites</NavLink>
       {loading === 'idle' && <NavCollections />}
       {/* In the future, will be conditionally rendered */}
