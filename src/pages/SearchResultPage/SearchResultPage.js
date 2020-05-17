d8d9acfdeeea91a1036ff92fb247e6d28859fd07
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-17 10:43:04 -0700

    setup

--- src/pages/SearchResultPage/SearchResultPage.js
+++ src/pages/SearchResultPage/SearchResultPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import { useSelector, useDispatch } from 'react-redux';
+import npms from '../../services/npmsAPI';
+import InfiniteScroll from 'react-infinite-scroller';
 import PackageList from '../../components/PackageList/PackageList';
 import ErrorBoundary from '../../ErrorBoundary';
 import { fetchPackages } from '../../redux/SearchResultsSlice';
@@ -19,13 +21,22 @@ function SearchResultPage() {
     dispatch(fetchPackages(parsed.q));
   }, [searchResults.searchTerm]);
 
+  const loadMore = () => {};
+
   return (
     <ErrorBoundary>
       <section>
-        {searchResults.loading === 'idle' && (
-          <PackageList packs={searchResults.packs} />
-        )}
-        {searchResults.loading === 'pending' && <p>Loading...</p>}
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={() => console.log('load more')}
+          hasMore={true || false}
+          loader={<p>Loading...</p>}
+        >
+          {searchResults.packs.length > 0 && (
+            <PackageList packs={searchResults.packs} />
+          )}
+          {searchResults.loading === 'pending' && <p>Loading...</p>}
+        </InfiniteScroll>
       </section>
     </ErrorBoundary>
   );
