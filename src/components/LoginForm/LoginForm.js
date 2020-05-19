047f6a9acf3b354650bdd1899331fe706d8e17f6
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-18 22:51:30 -0600

    Refactor loginform to use react-hooks-form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { useForm } from 'react-hook-form';
 import AuthService from '../../services/auth-api-service';
 import TokenService from '../../services/token-service';
 import { getCollections } from '../../redux/CollectionListSlice';
@@ -9,45 +10,71 @@ import './LoginForm.css';
 function LoginForm() {
   const history = useHistory();
   const dispatch = useDispatch();
+  const { register, handleSubmit, errors } = useForm();
 
-  // const login = () => {
-  //   AuthService.postLogin({
-  //     email: inputs.email,
-  //     password: inputs.password,
-  //   })
-  //     .then((res) => {
-  //       TokenService.saveAuthToken(res.authToken);
-  //     })
-  //     .then((res) => {
-  //       dispatch(getCollections());
-  //       history.push('/');
-  //     });
-  // };
+  const onSubmit = (data) => {
+    AuthService.postLogin({
+      email: data.email,
+      password: data.password,
+    })
+      .then((res) => {
+        TokenService.saveAuthToken(res.authToken);
+      })
+      .then((res) => {
+        dispatch(getCollections());
+        history.push('/');
+      });
+  };
 
-  function validateEmail(email) {
-    if (!email) {
-      return 'Please enter your email';
-    }
-    if (email > 40) {
-      return 'Is that a real email?';
-    }
-    return false;
-  }
-
-  function validatePassword(pass) {
-    if (!pass) {
-      return 'Please enter your password';
-    }
-    if (pass > 40) {
-      return 'Password cannot be longer than 40 characters';
-    }
-    if (pass < 6) {
-      return 'Password needs to be at least 6 characters long';
-    }
-    return false;
-  }
-
-  return <form>empty</form>;
+  return (
+    <form id="loginForm" onSubmit={handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        placeholder="Email"
+        name="email"
+        autoComplete="email"
+        ref={register({
+          required: true,
+          minLength: {
+            value: 9,
+            message: 'An email is usually longer than that.',
+          },
+          maxLength: {
+            value: 40,
+            message: `An email usually isn't that long.`,
+          },
+          pattern: {
+            value: /^\S+@\S+$/i,
+            message: `That doesn't seem to be a valid email.`,
+          },
+        })}
+      />
+      {errors.email && (
+        <p className="validationWarning">{errors.email.message}</p>
+      )}
+      <input
+        type="password"
+        placeholder="Password"
+        autoComplete="new-password"
+        name="password"
+        ref={register({
+          required: 'Please enter your password',
+          minLength: {
+            value: 6,
+            message: 'Password must be at least 6 characters long',
+          },
+          maxLength: {
+            value: 40,
+            message: 'Password cannot be longer than 40 characters',
+          },
+        })}
+      />
+      {errors.password && (
+        <p className="validationWarning">{errors.password.message}</p>
+      )}
+      <button type="submit">Log In</button>
+    </form>
+  );
 }
 
 export default LoginForm;
