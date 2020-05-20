0f2b527e0de8b2669cfda4a88af578e4469c8608
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-19 23:03:43 -0600

    Rough out getting started section

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -16,13 +16,26 @@ function LandingPage() {
           searchButton="Search"
           searchInputClass="landingSearchInput"
         />
-        <h2 className="landingFullName">
-          Managing <span className="underline">the</span> Manager
-        </h2>
-        <p className="description">
-          Node Package Manager Manager (npmm) is an app designed to help you
-          manage your favorite npm packages
-        </p>
+        <section className="landingIntro">
+          <h2 className="landingFullName">
+            Managing <span className="underline">the</span> Manager
+          </h2>
+          <p className="description">
+            Node Package Manager Manager (npmm) is an app designed to help you
+            manage your favorite npm packages
+          </p>
+        </section>
+        <section className="landingGettingStarted">
+          <h2 className="landingFullName">Getting Started</h2>
+          <p className="description">Install our CLI tool</p>
+          <div className="codeContainer">
+            <code>npm i @npmmjs/npmm</code>
+          </div>
+          <p className="description">
+            From there either navigate the site or use the CLI to find,
+            favorite, and collect your most used packages from npm
+          </p>
+        </section>
       </div>
     </ErrorBoundary>
   );
