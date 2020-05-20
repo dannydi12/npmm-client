8160c065eeadce4e2dcd194ff437ec6d35894420
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-19 20:15:01 -0600

    Clean SideBar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import NavCollections from '../NavCollections/NavCollections';
-// import TokenService from '../../services/token-service'
-// import {all the reducers/actions} from './sliceFile.js';
-// import styles from './example.css';
-
-// Use absolute imports like this for components!
-// import SearchBox from 'components/SearchBox/SearchBox';
 
 function SideBar() {
   const loading = useSelector((state) => state.collectionList.loading);
