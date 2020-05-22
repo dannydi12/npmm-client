930d4b688945dd59d174105a9916ad805387c984
Josh Young <joshua@young.net>
2020-05-22 11:27:23 -0600

    package card styling

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SignupForm from '../../components/SignupForm/SignupForm';
 import ErrorBoundary from '../../ErrorBoundary';
 import './SignupPage.css';
+import { Link } from 'react-router-dom';
 
 export default function SignupPage() {
   return (
@@ -10,6 +11,12 @@ export default function SignupPage() {
         <h2 className="pageTitle">Sign Up</h2>
         <p className="pageInstructions">
           Enter an email and password to register.
+        </p>
+        <p className="pageInstructions demoCredentials">
+          To demo the site without registering, just head over to the{' '}
+          <Link to="/login" className="defaultMedium linkBlack">
+            login page.
+          </Link>
         </p>
         <SignupForm />
       </div>
