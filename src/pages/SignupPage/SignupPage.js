e0159cd0fe0017210d4b7093832197561a9dac36
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-22 13:54:25 -0700

    fixed import order

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SignupForm from '../../components/SignupForm/SignupForm';
 import ErrorBoundary from '../../ErrorBoundary';
 import './SignupPage.css';
-import { Link } from 'react-router-dom';
 
 export default function SignupPage() {
   return (
