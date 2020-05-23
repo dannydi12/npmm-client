0afa09980da71041215e8067c51fdd20fb6efa32
Michael Ploughman <michaelhploughman@gmail.com>
2020-05-23 10:57:59 -0600

    Fix tests related to react-hooks-forms

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -10,7 +10,7 @@ test('renders login form without crashing', () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
-        <LoginForm />
+        <LoginForm setLoginError={() => false} />
       </BrowserRouter>
     </Provider>,
     div
