1fd0bbd3a0761ad08f14fb3b645b664391ec2269
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-26 11:12:19 -0700

    final touches

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -13,9 +13,6 @@ const TokenService = {
   hasAuthToken() {
     return !!TokenService.getAuthToken();
   },
-  makeBasicAuthToken(userName, password) {
-    return window.btoa(`${userName}:${password}`);
-  },
 };
 
 export default TokenService;
