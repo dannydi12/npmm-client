b6622aeda6f5bf8dbe1e9b4883f278f8676d6c52
Daniel Di Venere <danieldivenere@gmail.com>
2020-05-26 11:13:15 -0700

    removed old es lint exception

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom/extend-expect';
-// eslint-disable-next-line import/no-unresolved
 import 'mutationobserver-shim';
 
 global.MutationObserver = window.MutationObserver;
